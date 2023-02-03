335161fad9b8ff836a83141aa378a8abe3a13c9d
Damir Novalic <damir@Damirs-MacBook-Pro.local>
2023-02-03 22:54:30 +0400

    update the server url

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ import { Configuration, OpenAIApi } from 'openai';
 dotenv.config();
 
 const configuration = new Configuration({
-    apiKey: process.env.OPEN_API_KEY,
+    apiKey: process.env.OPEN_AI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
